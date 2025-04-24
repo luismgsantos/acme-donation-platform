c4f9970997178de792f5b7ed17fe3e6f393b38bc
Luís Santos <luis.santos.ext@tlscontact.com>
2025-04-24 19:27:14 +0100

    added migrations for sanctum and notifications

---
+++ database/migrations/2025_04_24_160616_create_personal_access_tokens_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('tokenable'); // tokenable_id and tokenable_type columns
+            $table->string('name');
+            $table->text('token');
+            $table->json('abilities')->nullable();
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('personal_access_tokens');
+    }
+};
