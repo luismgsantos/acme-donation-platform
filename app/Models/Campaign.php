78deee540704e9d6bf63df00d74f52c695d913af
Luís Santos <luis.santos.ext@tlscontact.com>
2025-04-24 19:28:44 +0100

    update docs on models

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -6,6 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * Entity Campaign
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $title
+ * @property string $description
+ * @property float $goal_amount
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ *
+ * @property-read \App\Models\User $user
+ */
 class Campaign extends Model
 {
     public function user(): BelongsTo
--- app/Models/Donation.php
+++ app/Models/Donation.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * Entity Donation
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $campaign_id
+ * @property float $amount
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ *
+ * @property-read \App\Models\User $user
+ * @property-read \App\Models\Campaign $campaign
+ */
 class Donation extends Model
 {
     public function user(): BelongsTo
