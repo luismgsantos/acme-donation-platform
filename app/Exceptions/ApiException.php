4eab4a76e8f2807b0dae4f7f8ee37343483e54e2
Luís Santos <luis.santos.ext@tlscontact.com>
2025-04-29 18:37:36 +0100

    refactor ApiException to use constants for HTTP status codes

--- app/Exceptions/ApiException.php
+++ app/Exceptions/ApiException.php
@@ -4,10 +4,11 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class ApiException extends Exception
 {
-    public function __construct(string $message, protected int $status = 400)
+    public function __construct(string $message, protected int $status = Response::HTTP_BAD_REQUEST)
     {
         parent::__construct($message);
     }
@@ -17,5 +18,15 @@ class ApiException extends Exception
         return response()->json([
             'error' => $this->getMessage(),
         ], $this->status);
+    }
+
+    public static function notFound(string $message): JsonResponse
+    {
+        return (new self($message, Response::HTTP_NOT_FOUND))->render();
+    }
+
+    public static function unauthorized(string $message = "Unauthorized."): JsonResponse
+    {
+        return (new self($message, Response::HTTP_UNAUTHORIZED))->render();
     }
 }
