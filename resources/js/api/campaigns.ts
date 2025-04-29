d709173919c407695a0ca656b2035f5da93355ca
Luís Santos <luis.santos.ext@tlscontact.com>
2025-04-29 18:39:03 +0100

    added missing implementations of put and delete methods for frontend

--- resources/js/api/campaigns.ts
+++ resources/js/api/campaigns.ts
@@ -1,6 +1,7 @@
+import { Campaign } from '@/types';
 import axios from 'axios';
 
-export async function createCampaign(data: { title: string; description: string; goal_amount: number }) {
+export async function createCampaign(data: Campaign) {
     return axios.post(route('api.campaigns.store'), data);
 }
 
@@ -10,4 +11,12 @@ export async function getCampaigns() {
 
 export async function getCampaign(id: number) {
     return axios.get(route('api.campaigns.show', id));
+}
+
+export async function updateCampaign(id: number, data: Campaign) {
+    return axios.put(route('api.campaigns.update', id), data);
+}
+
+export async function deleteCampaign(id: number) {
+    return axios.delete(route('api.campaigns.destroy', id));
 }
