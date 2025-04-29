afdd777cfeedc3e06fe1b80ac520049209c824f1
Luís Santos <luis.santos.ext@tlscontact.com>
2025-04-29 10:53:24 +0100

    personalize with project name instead of framework auto generated boilerplate

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import AppLayout from '@/layouts/app/AppSidebarLayout.vue';
+import AppLayout from '@/layouts/app/AppHeaderLayout.vue';
 import type { BreadcrumbItemType } from '@/types';
-
 interface Props {
     breadcrumbs?: BreadcrumbItemType[];
 }
