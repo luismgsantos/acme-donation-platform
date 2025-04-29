ea3ec32ee71a4a40367d40e8b40d4541061309d5
Luís Santos <luis.santos.ext@tlscontact.com>
2025-04-29 10:50:34 +0100

    added database seeding to do some manual testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Campaign;
+use App\Models\Donation;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Campaign::factory(10)->create();
+
+        Donation::factory(100)->create();
     }
 }
